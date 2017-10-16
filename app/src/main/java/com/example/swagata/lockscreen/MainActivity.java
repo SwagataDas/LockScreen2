15c5e82fc76cb15475b487fab11a1df6ad445308
Sayan <msayanece@gmail.com>
2017-10-17 00:13:32 +0530

    add button click function with creating next activity for imageview

--- app/src/main/java/com/example/swagata/lockscreen/MainActivity.java
+++ app/src/main/java/com/example/swagata/lockscreen/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.swagata.lockscreen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = (TextView) findViewById(R.id.textview);
+    }
 
+    public void numberOnClick(View view) {
+        text.append("*");
+        if (text.getText().length()>3) {
+            startActivity(new Intent(this, ImageActivity.class));
+        }
     }
 }
